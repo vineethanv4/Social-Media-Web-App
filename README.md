8ea5bd10a27fbdfe05acfa15fa343f47328445b5
vineethanv4 <vineethanv4@gmail.com>
2023-08-31 15:31:24 +0530

    Project Files

---
+++ README.md
@@ -0,0 +1,3 @@
+# social media app
+A Social Media application developed using MERN stack which supports basic features sharing posts, following users, view status of friends etc.
+Also added chat feature using socket.io
---
+++ client/src/components/chatOnline/ChatOnline.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./chatOnline.css";
+
+export default function ChatOnline({ onlineUsers, currentId, setCurrentChat }) {
+  const [friends, setFriends] = useState([]);
+  const [onlineFriends, setOnlineFriends] = useState([]);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  useEffect(() => {
+    const getFriends = async () => {
+      const res = await axios.get("/users/friends/" + currentId);
+      setFriends(res.data);
+    };
+
+    getFriends();
+  }, [currentId]);
+
+  useEffect(() => {
+    setOnlineFriends(friends.filter((f) => onlineUsers.includes(f._id)));
+  }, [friends, onlineUsers]);
+
+  const handleClick = async (user) => {
+    try {
+      const res = await axios.get(
+        `/conversations/find/${currentId}/${user._id}`
+      );
+      if (res.data) {
+        setCurrentChat(res.data);
+      } else {
+        const anotherRes = await axios.post(`/conversations/`, {
+          senderId: currentId,   // Replace with the appropriate sender ID
+          receiverId: user._id   // Replace with the appropriate receiver ID
+        });
+        const updatedRes = await axios.get(`/conversations/find/${currentId}/${user._id}`);
+        setCurrentChat(updatedRes.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="chatOnline">
+      {onlineFriends.map((o) => (
+        <div className="chatOnlineFriend" onClick={() => handleClick(o)}>
+          <div className="chatOnlineImgContainer">
+            <img
+              className="chatOnlineImg"
+              src={
+                o?.profilePicture
+                  ? PF + o.profilePicture
+                  : PF + "person/noAvatar.png"
+              }
+              alt=""
+            />
+            <div className="chatOnlineBadge"></div>
+          </div>
+          <span className="chatOnlineName">{o?.username}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
